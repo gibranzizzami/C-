d20ca2b548f97c0ccedc607c03cb5f8cd8dcd884
gibranzizzami <gbrnzzmi24@gmail.com>
2026-02-17 13:46:30 +0700

    update

---
+++ learning.cpp
@@ -0,0 +1,10 @@
+// make folder
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <iostream>
+
+int main() {
+    mkdir("FolderBaru", 0777);
+    std::cout << "Folder berhasil dibuat!" << std::endl;
+    return 0;
+}
