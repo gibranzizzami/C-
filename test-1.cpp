17ee61a01e9ac2554f06187418515a06a1ca682c
gibranzizzami <gbrnzzmi24@gmail.com>
2026-02-17 16:41:20 +0700

    update

--- test-1.cpp
+++ test-1.cpp
@@ -1,16 +1,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <iostream>
-#include <fstream>
 
 int main() {
-    // made folder
     mkdir("FolderBaru", 0777);
     std::cout << "Folder berhasil dibuat!" << std::endl;
-}
-
-    // made file
-     std::ofstream file("contoh.txt");
-     cout << "file berhasil dibuat";
-     return 0;
+    return 0;
 }
---
+++ test-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+    std::offstream file("test.md");
+    cout << "file berhasil dibuat";
+    return 0;
+}
