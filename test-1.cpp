6bb76af71d85928025b4a18bca2ce186d76dccb1
gibranzizzami <gbrnzzmi24@gmail.com>
2026-02-18 15:42:09 +0700

    update

--- test-1.cpp
+++ test-1.cpp
@@ -1,10 +1,17 @@
-// buat folder
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
-    mkdir("FolderBaru", 0777);
-    std::cout << "Folder berhasil dibuat!" << std::endl;
+    std::string folderName = "folder-saya";
+
+    // Membuat folder
+    if (fs::create_directory(folderName)) {
+        std::cout << "folder berhasil dibuat: " << folderName << std::endl;
+    } else {
+        std::cout << "folder gagal dibuat atau sudah ada." << std::endl;
+    }
+
     return 0;
 }
